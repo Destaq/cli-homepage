a0f7171da63876e389b5c6c6fb68f2de5d87bfb0
Simon Ilincev <trilogicworlds@gmail.com>
2023-05-06 14:36:38 +0200

    fix: re-add useTheme import

--- src/utils/shellProvider.tsx
+++ src/utils/shellProvider.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { History } from "../interfaces/history";
 import * as bin from "./bin";
+import { useTheme } from "./themeProvider";
 
 interface ShellContextType {
 	history: History[];
@@ -33,6 +34,7 @@ export const ShellProvider: React.FC<ShellProviderProps> = ({ children }) => {
 	const [command, _setCommand] = React.useState<string>("");
 	const [dynamicCommand, _setDynamicCommand] = React.useState<string>("");
 	const [lastCommandIndex, _setLastCommandIndex] = React.useState<number>(0);
+	const { setTheme } = useTheme();
 
 	useEffect(() => {
 		setCommand("banner");
